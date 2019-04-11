7b9f9a699c3fbb36ad47b84502e5f63b55c9252e
Pavlo <pilmen@gmail.com>
2019-04-11 19:37:09 -0400

    Xcode project initialization

---
+++ RedditTestTests/RedditTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  RedditTestTests.swift
+//  RedditTestTests
+//
+//  Created by Pavlo Naumenko on 4/11/19.
+//  Copyright © 2019 Pavlo Naumenko. All rights reserved.
+//
+
+import XCTest
+@testable import RedditTest
+
+class RedditTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
